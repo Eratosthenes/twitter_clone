8834600b4ba9e4bc1c40c8ad544d657b4e4cb2df
Nathan Rapport <n.rapport@gmail.com>
2015-11-24 13:35:49 -0800

    added user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,13 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
 end
   #get 'static_pages/home'
