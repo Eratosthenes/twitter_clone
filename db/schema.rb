2dd5d80c7544cefe32a311c31e94441e9b42e8de
Nathan Rapport <n.rapport@gmail.com>
2015-11-23 20:13:17 -0800

    messed w/ migrations

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20151124021137) do
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
